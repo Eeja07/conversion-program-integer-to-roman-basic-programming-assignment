70025aac12526f43d1a3611a7aa4f22377656d58
Eeja <mahijapradipta86@gmail.com>
2023-07-18 05:42:24 +0700

    Fix code

---
+++ Tugas1.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+int main()
+{
+    int input;
+    do
+    {
+        printf("\n\nProgram Konversi Bilangan Bulat Ke Romawi");
+        printf("\nMasukkan Bilangan Bulat = ");
+        scanf(" %d", &input);
+        if (input < 1 || input > 4999)
+        {
+            printf("Masukkan Ulang Direntang 1-4999\n");
+        }
+        else
+        {
+            while (input > 0)
+                if (input >= 1000)
+                {
+                    printf("M");
+                    input -= 1000;
+                }
+
+                else if (input >= 900)
+                {
+                    printf("CM");
+                    input -= 900;
+                }
+                else if (input >= 500)
+                {
+                    printf("D");
+                    input -= 500;
+                }
+                else if (input >= 400)
+                {
+                    printf("CD");
+                    input -= 400;
+                }
+                else if (input >= 100)
+                {
+                    printf("C");
+                    input -= 100;
+                }
+                else if (input >= 90)
+                {
+                    printf("XC");
+                    input -= 90;
+                }
+                else if (input >= 50)
+                {
+                    printf("L");
+                    input -= 50;
+                }
+                else if (input >= 40)
+                {
+                    printf("XL");
+                    input -= 40;
+                }
+                else if (input >= 10)
+                {
+                    printf("X");
+                    input -= 10;
+                }
+                else if (input >= 9)
+                {
+                    printf("IX");
+                    input -= 9;
+                }
+                else if (input >= 5)
+                {
+                    printf("V");
+                    input -= 5;
+                }
+                else if (input >= 4)
+                {
+                    printf("IV");
+                    input -= 4;
+                }
+                else if (input >= 1)
+                {
+                    printf("I");
+                    input -= 1;
+                }
+        }
+    } while (input <= 0 || input >= 0);
+}
